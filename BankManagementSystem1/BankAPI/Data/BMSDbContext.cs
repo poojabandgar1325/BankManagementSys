b479b84a5126601b1b6e704c5d711907d6bdb099
Poojabandgar1325 <poojabandgar1325@gmail.com>
2022-11-29 23:01:18 +0530

    Initial setup1

---
+++ BankManagementSystem1/BankAPI/Data/BMSDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BankAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAPI.Data
+{
+    public class BMSDbContext : DbContext
+    {
+        public BMSDbContext(DbContextOptions<BMSDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserDetail> UserDetails { get; set; }
+        public DbSet<LoanDetail> LoanDetails { get; set; }
+    
+    }
+}
